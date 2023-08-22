a276d76f43e55b6315d04667a7d4741078bde3dc
Rahul Patni <rahul@iflabs.network>
2023-08-22 11:19:38 -0700

    connecting to node when db is open

--- src/commands/debug.ts
+++ src/commands/debug.ts
@@ -49,6 +49,7 @@ export default class Debug extends IronfishCommand {
 
     let output = this.baseOutput(node)
     if (dbOpen) {
+      await node.connectRpc()
       output = new Map([...output, ...(await this.outputRequiringDB(node))])
     }
 
