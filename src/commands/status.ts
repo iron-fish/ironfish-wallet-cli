aac20ba28d329f7e66276e326d4dd5b420d4eb29
Rahul Patni <rahul@iflabs.network>
2023-08-25 16:35:52 -0700

    adding wallet status -f flag

--- src/commands/status.ts
+++ src/commands/status.ts
@@ -1,7 +1,14 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import { CliUx } from '@oclif/core'
+import {
+  GetAccountStatusResponse,
+  PromiseUtils,
+  RpcResponseEnded,
+} from '@ironfish/sdk'
+import { CliUx, Flags } from '@oclif/core'
+import { FlagInput } from '@oclif/core/lib/interfaces'
+import blessed from 'blessed'
 import { IronfishCommand } from '../command'
 import { RemoteFlags } from '../flags'
 import { connectRpcWallet } from '../utils/clients'
@@ -12,18 +19,67 @@ export class StatusCommand extends IronfishCommand {
   static flags = {
     ...RemoteFlags,
     ...CliUx.ux.table.flags(),
+    follow: Flags.boolean({
+      char: 'f',
+      default: false,
+      description: 'Follow the status of the node live',
+    }),
   }
 
   async start(): Promise<void> {
     const { args, flags } = await this.parse(StatusCommand)
+    const follow = flags.follow as boolean | undefined
+
     const account = args.account as string | undefined
 
-    const client = await connectRpcWallet(this.sdk)
+    if (!follow) {
+      const client = await connectRpcWallet(this.sdk)
 
-    const response = await client.wallet.getAccountsStatus({
-      account: account,
-    })
+      const response = await client.wallet.getAccountsStatus({
+        account: account,
+      })
 
+      this.renderStatus(response, flags, this.log.bind(this))
+
+      this.exit(0)
+    }
+
+    // Console log will create display issues with Blessed
+    this.logger.pauseLogs()
+
+    const screen = blessed.screen({ smartCSR: true, fullUnicode: true })
+    const statusText = blessed.text()
+    screen.append(statusText)
+
+    while (true) {
+      statusText.clearBaseLine(0)
+
+      const client = await connectRpcWallet(this.sdk)
+
+      await client.wallet.getAccountsStatus({
+        account: account,
+      })
+
+      const response = await client.wallet.getAccountsStatus({
+        account: account,
+      })
+
+      const logTable = (s: string) => {
+        statusText.insertBottom(s)
+      }
+
+      this.renderStatus(response, flags, logTable)
+
+      screen.render()
+      await PromiseUtils.sleep(1000)
+    }
+  }
+
+  renderStatus(
+    response: RpcResponseEnded<GetAccountStatusResponse>,
+    flags: FlagInput,
+    printLine: (s: string) => void,
+  ): void {
     CliUx.ux.table(
       response.content.accounts,
       {
@@ -45,7 +101,7 @@ export class StatusCommand extends IronfishCommand {
         },
       },
       {
-        printLine: this.log.bind(this),
+        printLine: printLine,
         ...flags,
       },
     )
