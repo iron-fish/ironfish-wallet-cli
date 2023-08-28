7e48d5b100516b6177a1dd0f8e127f97f05a1973
Rohan Jadvani <5459049+rohanjadvani@users.noreply.github.com>
2023-08-28 16:15:01 -0400

    fix(wallet-cli): Remove telemetry log in start

--- src/commands/repl.ts
+++ src/commands/repl.ts
@@ -1,11 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import {
-  ApiNamespace,
-  NodeUtils,
-  RpcMemoryClient,
-} from '@ironfish/sdk'
+import { ApiNamespace, NodeUtils, RpcMemoryClient } from '@ironfish/sdk'
 import { Flags } from '@oclif/core'
 import fs from 'fs/promises'
 import repl from 'node:repl'
