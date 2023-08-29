b3af71800f09cce61c10373c449abf1ffcad4c4a
Rahul Patni <patnir@users.noreply.github.com>
2023-08-29 15:53:09 -0700

    Rahul/ifl 1504 add f flag to walletstatus nice to have (#26)
    * adding wallet status -f flag
    
    * displaying table at the top of the page
    
    * adding comments about cli ux table
    
    * Update src/commands/status.ts
    
    Co-authored-by: Rohan Jadvani <5459049+rohanjadvani@users.noreply.github.com>
    
    * changing log table to a variable
    
    * changing printline object
    
    * Adding newline for fix
    
    ---------
    
    Co-authored-by: Rohan Jadvani <5459049+rohanjadvani@users.noreply.github.com>

--- src/commands/status.ts
+++ src/commands/status.ts
@@ -1,7 +1,14 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import { CliUx } from '@oclif/core'
+import {
+  GetAccountStatusResponse,
+  PromiseUtils,
+  RpcResponseEnded,
+} from '@ironfish/sdk'
+import { CliUx, Flags } from '@oclif/core'
+import { FlagInput } from '@oclif/core/lib/interfaces'
+import blessed from 'blessed'
 import { IronfishCommand } from '../command'
 import { RemoteFlags } from '../flags'
 import { connectRpcWallet } from '../utils/clients'
@@ -12,18 +19,92 @@ export class StatusCommand extends IronfishCommand {
   static flags = {
     ...RemoteFlags,
     ...CliUx.ux.table.flags(),
+    follow: Flags.boolean({
+      char: 'f',
+      default: false,
+      description: 'Follow the status of the node live',
+    }),
   }
 
   async start(): Promise<void> {
     const { args, flags } = await this.parse(StatusCommand)
+    const follow = flags.follow as boolean | undefined
+
     const account = args.account as string | undefined
 
-    const client = await connectRpcWallet(this.sdk)
+    if (!follow) {
+      const client = await connectRpcWallet(this.sdk)
 
-    const response = await client.wallet.getAccountsStatus({
-      account: account,
-    })
+      const response = await client.wallet.getAccountsStatus({
+        account,
+      })
 
+      this.renderStatus(response, flags, this.log.bind(this))
+
+      this.exit(0)
+    }
+
+    // Console log will create display issues with Blessed
+    this.logger.pauseLogs()
+
+    const screen = blessed.screen({ smartCSR: true, fullUnicode: true })
+    const statusText = blessed.text()
+    screen.append(statusText)
+
+    let previousResponse = ''
+
+    while (true) {
+      const connected = await this.sdk.client.tryConnect()
+
+      if (!connected) {
+        statusText.clearBaseLine(0)
+
+        if (previousResponse) {
+          statusText.setContent(previousResponse)
+
+          statusText.insertTop('Node: Disconnected \n')
+        } else {
+          statusText.setContent('Node: STOPPED')
+        }
+
+        screen.render()
+        await PromiseUtils.sleep(1000)
+        continue
+      }
+
+      statusText.clearBaseLine(0)
+
+      const client = await connectRpcWallet(this.sdk)
+
+      const response = await client.wallet.getAccountsStatus({
+        account: account,
+      })
+
+      let tableBody = ''
+
+      // This is a workaround to display the CliUX.Table output in Blessed.
+      // CliUX.Table does not return a string and instead prints to a custom function
+
+      const logTable = (s: string) => {
+        tableBody += s + '\n'
+      }
+
+      this.renderStatus(response, flags, logTable)
+
+      statusText.setContent(tableBody)
+
+      previousResponse = tableBody
+
+      screen.render()
+      await PromiseUtils.sleep(1000)
+    }
+  }
+
+  renderStatus(
+    response: RpcResponseEnded<GetAccountStatusResponse>,
+    flags: FlagInput,
+    printLine: (s: string) => void,
+  ): void {
     CliUx.ux.table(
       response.content.accounts,
       {
@@ -45,7 +126,7 @@ export class StatusCommand extends IronfishCommand {
         },
       },
       {
-        printLine: this.log.bind(this),
+        printLine,
         ...flags,
       },
     )
