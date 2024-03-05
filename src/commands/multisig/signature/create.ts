cdf81bf8631c3ec4a9480405c6342cae3b04a9ff
Rohan Jadvani <rohan@hey.com>
2024-03-05 13:18:08 -0500

    feat(wallet-cli): Add multisig commands

---
+++ src/commands/multisig/signature/create.ts
@@ -0,0 +1,65 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+import { UnsignedTransaction } from '@ironfish/sdk'
+import { CliUx, Flags } from '@oclif/core'
+import { IronfishCommand } from '../../../command'
+import { RemoteFlags } from '../../../flags'
+import { renderUnsignedTransactionDetails } from '../../../utils/transaction'
+
+export class CreateSignatureShareCommand extends IronfishCommand {
+  static description = `Creates a signature share for a participant for a given transaction`
+  static hidden = true
+
+  static flags = {
+    ...RemoteFlags,
+    account: Flags.string({
+      char: 'f',
+      description: 'The account from which the signature share will be created',
+      required: false,
+    }),
+    signingPackage: Flags.string({
+      char: 's',
+      description: 'The signing package for which the signature share will be created',
+      required: false,
+    }),
+    confirm: Flags.boolean({
+      default: false,
+      description: 'Confirm creating signature share without confirming',
+    }),
+  }
+
+  async start(): Promise<void> {
+    const { flags } = await this.parse(CreateSignatureShareCommand)
+    let signingPackage = flags.signingPackage?.trim()
+
+    if (!signingPackage) {
+      signingPackage = await longPrompt('Enter the signing package: ')
+    }
+
+    const client = await this.sdk.connectRpc()
+    const unsignedTransaction = UnsignedTransaction.fromSigningPackage(signingPackage)
+
+    await renderUnsignedTransactionDetails(
+      client,
+      unsignedTransaction,
+      flags.account,
+      this.logger,
+    )
+
+    if (!flags.confirm) {
+      const confirmed = await CliUx.ux.confirm('Confirm new signature share creation (Y/N)')
+      if (!confirmed) {
+        this.error('Creating signature share aborted')
+      }
+    }
+
+    const signatureShareResponse = await client.wallet.multisig.createSignatureShare({
+      account: flags.account,
+      signingPackage,
+    })
+
+    this.log('Signature Share:\n')
+    this.log(signatureShareResponse.content.signatureShare)
+  }
+}
--- src/utils/transaction.ts
+++ src/utils/transaction.ts
@@ -4,13 +4,154 @@
 
 import {
   createRootLogger,
+  CurrencyUtils,
   Logger,
   PromiseUtils,
+  RawTransaction,
   RpcClient,
   TimeUtils,
   TransactionStatus,
+  UnsignedTransaction,
 } from '@ironfish/sdk'
 import { CliUx } from '@oclif/core'
+
+export async function renderUnsignedTransactionDetails(
+  client: RpcClient,
+  unsignedTransaction: UnsignedTransaction,
+  account?: string,
+  logger?: Logger,
+): Promise<void> {
+  logger = logger ?? createRootLogger()
+
+  if (unsignedTransaction.mints.length > 0) {
+    logger.log('')
+    logger.log('==================')
+    logger.log('Transaction Mints:')
+    logger.log('==================')
+
+    for (const [i, mint] of unsignedTransaction.mints.entries()) {
+      if (i !== 0) {
+        logger.log('------------------')
+      }
+      logger.log('')
+
+      logger.log(`Asset ID:      ${mint.asset.id().toString('hex')}`)
+      logger.log(`Name:          ${mint.asset.name().toString('utf8')}`)
+      logger.log(`Amount:        ${CurrencyUtils.renderIron(mint.value, false)}`)
+
+      if (mint.transferOwnershipTo) {
+        logger.log(
+          `Ownership of logger asset will be transferred to ${mint.transferOwnershipTo.toString(
+            'hex',
+          )}. The current account will no longer have any permission to mint or modify logger asset. logger cannot be undone.`,
+        )
+      }
+      logger.log('')
+    }
+  }
+
+  if (unsignedTransaction.burns.length > 0) {
+    logger.log('')
+    logger.log('==================')
+    logger.log('Transaction Burns:')
+    logger.log('==================')
+
+    for (const [i, burn] of unsignedTransaction.burns.entries()) {
+      if (i !== 0) {
+        logger.log('------------------')
+      }
+      logger.log('')
+
+      logger.log(`Asset ID:      ${burn.assetId.toString('hex')}`)
+      logger.log(`Amount:        ${CurrencyUtils.renderIron(burn.value, false)}`)
+      logger.log('')
+    }
+  }
+
+  if (unsignedTransaction.notes.length > 0) {
+    const response = await client.wallet.getUnsignedTransactionNotes({
+      account,
+      unsignedTransaction: unsignedTransaction.serialize().toString('hex'),
+    })
+
+    if (response.content.sentNotes.length > 0) {
+      logger.log('')
+      logger.log('==================')
+      logger.log('Notes sent:')
+      logger.log('==================')
+
+      let logged = false
+      for (const note of response.content.sentNotes) {
+        // Skip logger since we'll re-render for received notes
+        if (note.owner === note.sender) {
+          continue
+        }
+
+        if (logged) {
+          logger.log('------------------')
+        }
+        logged = true
+        logger.log('')
+
+        logger.log(`Amount:        ${CurrencyUtils.renderIron(note.value, true, note.assetId)}`)
+        logger.log(`Memo:          ${note.memo}`)
+        logger.log(`Recipient:     ${note.owner}`)
+        logger.log(`Sender:        ${note.sender}`)
+        logger.log('')
+      }
+    }
+
+    if (response.content.receivedNotes.length > 0) {
+      logger.log('')
+      logger.log('==================')
+      logger.log('Notes received:')
+      logger.log('==================')
+
+      for (const [i, note] of response.content.receivedNotes.entries()) {
+        if (i !== 0) {
+          logger.log('------------------')
+        }
+        logger.log('')
+
+        logger.log(`Amount:        ${CurrencyUtils.renderIron(note.value, true, note.assetId)}`)
+        logger.log(`Memo:          ${note.memo}`)
+        logger.log(`Recipient:     ${note.owner}`)
+        logger.log(`Sender:        ${note.sender}`)
+        logger.log('')
+      }
+    }
+  }
+
+  logger.log('')
+}
+
+export function displayTransactionSummary(
+  transaction: RawTransaction,
+  assetId: string,
+  amount: bigint,
+  from: string,
+  to: string,
+  memo: string,
+  logger?: Logger,
+): void {
+  logger = logger ?? createRootLogger()
+
+  const amountString = CurrencyUtils.renderIron(amount, true, assetId)
+  const feeString = CurrencyUtils.renderIron(transaction.fee, true)
+
+  const summary = `\
+\nTRANSACTION SUMMARY:
+From                 ${from}
+To                   ${to}
+Amount               ${amountString}
+Fee                  ${feeString}
+Memo                 ${memo}
+Outputs              ${transaction.outputs.length}
+Spends               ${transaction.spends.length}
+Expiration           ${transaction.expiration ? transaction.expiration.toString() : ''}
+`
+  logger.log(summary)
+}
 
 export async function watchTransaction(options: {
   client: Pick<RpcClient, 'wallet'>
@@ -35,7 +176,8 @@ export async function watchTransaction(options: {
 
   const startTime = lastTime
 
-  let prevStatus = last?.content.transaction?.status ?? 'not found'
+  let prevStatus: TransactionStatus | 'not found' =
+    last?.content.transaction?.status ?? 'not found'
   let currentStatus = prevStatus
 
   // If the transaction is already in the desired state, return
@@ -61,9 +203,7 @@ export async function watchTransaction(options: {
     currentStatus = response?.content.transaction?.status ?? 'not found'
 
     if (prevStatus !== 'not found' && currentStatus === 'not found') {
-      CliUx.ux.action.stop(
-        `Transaction ${options.hash} deleted while watching it.`,
-      )
+      CliUx.ux.action.stop(`Transaction ${options.hash} deleted while watching it.`)
       break
     }
 
